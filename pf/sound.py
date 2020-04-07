63b83c2e285fb3e70151aabd97c0163ea835390b
Janklod7 <kliko7@gmail.com>
2020-04-07 22:31:25 +0300

    updated the class Sound, mostly finish and its time to move to the phonological surrounding

--- pf/sound.py
+++ pf/sound.py
@@ -1,13 +1,14 @@
+from .features import FEATURES
 class Sound(object):
-    def __init__(self, content: str, type_: str, features: list):
+    def __init__(self, content: str, type_: str, features: dict):
             self.content = content
-            self.features = features
-            self.type = type_
+            self.features = self.translate_features(features)
+            self.type_ = type_
 						
     def __repr__(self, singular=False):
         output = self.content + " "
         if singular:
-            for feature in self.features:
+            for feature in self.features.values():
                 output += feature + " "
         return output[:-1]
 
@@ -16,6 +17,17 @@ class Sound(object):
 
     def compare(self, other):
         if self == other:
-            return True
-        for feature in self.features: # TODO: understand tomer's feature structure
-            return False
+            return []
+        if self.type_ != other.type_:
+            return ["TYPE"]
+        diffs = []
+        for feature in self.features.values():
+            if self.features[feature] != other.features[feature]:
+                diffs.append(feature)
+        return diffs
+
+    def getfeature(self,feature):
+        return self.features[feature]
+    def translate_features(self,features):
+        return features # TODO figure out
+    
